daf751b80be0d372cdd215f24adb347b22dbcea1
VietThanh1976 <165611983+VietThanh1976@users.noreply.github.com>
2025-11-22 22:49:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 # Gọi hàm login. Đặt form ở cột chính ('main')
-name, authentication_status, username = authenticator.login('Đăng nhập', 'main')
+name, authentication_status, username = authenticator.login('Login', 'main')
 
 # 3. Khởi tạo Session State cho ứng dụng chính ---
 r = sr.Recognizer()
@@ -223,5 +223,6 @@ elif authentication_status == None:
     st.info('Vui lòng nhập tên người dùng và mật khẩu của bạn')
 
     st.info("Sử dụng: **admin / vietthanh** hoặc **user1 / password**")
+
 
 
